66156c3b37b947aa7e46f10f088d6825baea62f9
Mitrea Andrei Razvan <razvanmitrea1@gmail.com>
2019-06-24 18:51:34 +0300

    schimbari

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -12,4 +12,35 @@ var API_METHOD = {
 fetch(API_URL.READ).then(function(r){
     return r.json()
 }).then(function(curse){
+    console.log('curse', curse);
+
+    //TODO - parcurgem lista de curse si generam cate un rand in tabel 
 })
+function display(curse){
+    console.log(curse);
+    const list = curse.map(function (cursele){
+        return `<tr data-id="${cursele.id}">
+        <td>${cursele.pornire}</td>
+        <td>${cursele.destantie}</td>
+        <td>${cursele.marfa}</td>
+        </tr>`;
+    })
+    document.querySelector('#curse tbody').innerHTML = list.JSON('');
+    myJSON = JSON.stringify(curse);
+}
+
+//2. function display(curse) {
+    //console.warn(curse);
+    //var list = curse.map(function (info) {
+        //return `<tr data-id="${info.id}">
+        //<td>${info.pornire}</td>
+        //<td>${info.destantie}</td>
+        //<td>${info.tip-marfa}</td>
+      //</tr>`;
+    //});
+    //document.querySelector("#curse tbody").innerHTML = list.json('');
+//}
+
+
+
+
